1b191cf30b0a3efd8cfe72fe29691ebcdecbd1da
spacefarers <michael@yangm.tech>
2024-01-11 17:39:25 +0800

    DA

--- DomainAdaptation.py
+++ DomainAdaptation.py
@@ -26,7 +26,7 @@ def DomainAdaptation(run_id=300, source_iters=100, target_iters=100, tag="DA", l
     Path(experiment_dir).mkdir(parents=True, exist_ok=True)
     source_ds = Dataset(config.source_dataset, config.source_var, "all")
     target_ds = Dataset(config.target_dataset, config.target_var, "train")
-    source_evaluate_every = 5
+    source_evaluate_every = 20
     target_evaluate_every = 20
 
 
--- tasks/DA.sh
+++ tasks/DA.sh
@@ -7,6 +7,6 @@
 
 conda activate ml
 
-python3 ../DomainAdaptation.py --run_id=$SGE_TASK_ID --stage="target" --use_restorer=True
+python3 ../DomainAdaptation.py --run_id=$SGE_TASK_ID --stage="all" --use_restorer=True
 
 # qsub -t 200 DA.sh
