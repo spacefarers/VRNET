3eecdd1e8cb34e3dfcce031b6382d165b38e7073
spacefarers <michael@yangm.tech>
2024-01-11 22:35:48 +0800

    Update DA.sh

--- tasks/DA.sh
+++ tasks/DA.sh
@@ -7,6 +7,6 @@
 
 conda activate ml
 
-python3 ../DomainAdaptation.py --run_id=$SGE_TASK_ID --stage="all" --use_restorer=True
+python3 ../DomainAdaptation.py --run_id=$SGE_TASK_ID --stage="all" --use_restorer=False
 
 # qsub -t 200 DA.sh
