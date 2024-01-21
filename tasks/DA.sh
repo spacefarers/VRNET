25f9e86bb09e9d224dbfa88f6132f1538da937c6
Michael Yang <michael@yangm.tech>
2024-01-21 11:09:47 -0500

    Update DA.sh

--- tasks/DA.sh
+++ tasks/DA.sh
@@ -7,6 +7,6 @@
 
 conda activate ml
 
-python3 ../DomainAdaptation.py --run_id=$SGE_TASK_ID --stage="target" --load_model=True
+python3 ../DomainAdaptation.py --run_id=$SGE_TASK_ID --stage="target" --load_model=False
 
 # qsub -t 200 DA.sh
