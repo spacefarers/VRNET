e17b03dce08b5e0ba0f82c0f74dd9ac589e081e4
Michael Yang <michael@yangm.tech>
2024-01-28 20:19:59 -0500

    Update DomainAdaptation.py

--- DomainAdaptation.py
+++ DomainAdaptation.py
@@ -67,16 +67,16 @@ def DomainAdaptation(run_id=40, source_iters=100, target_iters=200, tag="DA", lo
             source_data = source_ds.get_augmented_data()
             bp_crop_times = config.crop_times
             config.crop_times *= math.ceil(len(source_ds) / len(source_aid_ds))
-            target_data = iter(source_aid_ds.get_augmented_data())
+            # target_data = iter(source_aid_ds.get_augmented_data())
             config.crop_times = bp_crop_times
             M.train()
             for batch_idx, (low_res_source, high_res_source) in enumerate(
                     tqdm(source_data, leave=False, desc="Source Iters", position=1)):
                 low_res_source = low_res_source.to(config.device)
                 high_res_source = high_res_source.to(config.device)
-                target_low, target_high = next(target_data)
-                target_low = target_low.to(config.device)
-                target_high = target_high.to(config.device)
+                # target_low, target_high = next(target_data)
+                # target_low = target_low.to(config.device)
+                # target_high = target_high.to(config.device)
 
                 # # Train Discriminators
                 # M.encoder.requires_grad_(False)
@@ -106,14 +106,14 @@ def DomainAdaptation(run_id=40, source_iters=100, target_iters=200, tag="DA", lo
                 FDC.requires_grad_(False)
                 optimizer.zero_grad()
                 features_S = E(low_res_source[:, 0:1], low_res_source[:, -1:])
-                features_T = E(target_low[:, 0:1], target_low[:, -1:])
+                # features_T = E(target_low[:, 0:1], target_low[:, -1:])
                 source_hi = U(features_S)
                 # target_hi = U(features_T)
                 # feature_source_label = FDC(features_S)
                 # feature_target_label = FDC(features_T)
                 # features_label_loss = domain_criterion(feature_source_label, torch.full_like(feature_source_label, 0.5)) + domain_criterion(feature_target_label, torch.full_like(feature_target_label, 0.5))
                 # source_restore = R(features_S)
-                target_restore = R(features_T)
+                # target_restore = R(features_T)
                 # LR_target_label = LRDC(target_low)
                 # LR_source_label = LRDC(source_restore)
                 # LR_label_loss = domain_criterion(LR_target_label, torch.full_like(LR_target_label, 0.5)) + domain_criterion(LR_source_label, torch.full_like(LR_source_label, 0.5))
@@ -121,12 +121,12 @@ def DomainAdaptation(run_id=40, source_iters=100, target_iters=200, tag="DA", lo
                 # cycle_source_hi = U(cycle_source_feature)
                 # cycle_vol_loss = criterion(cycle_source_hi, high_res_source)
                 vol_loss = criterion(source_hi, high_res_source)
-                restore_loss = criterion(target_low, target_restore)
+                # restore_loss = criterion(target_low, target_restore)
                 # vol_loss_target = criterion(target_hi, target_high)
                 # source_identity_loss = criterion(cycle_source_feature, features_S)
                 # loss = 0.01*LR_label_loss + 0.01*features_label_loss + 0.01*source_identity_loss + vol_loss + cycle_vol_loss + restore_loss
                 # loss = 0.01*source_identity_loss + vol_loss + cycle_vol_loss + restore_loss
-                loss = vol_loss + restore_loss
+                loss = vol_loss
                 # config.track({"S1 LR Label Loss": LR_label_loss, "S1 Feature Label Loss": features_label_loss, "S1 Source Identity Loss": source_identity_loss, "S1 Vol Loss": vol_loss, "S1 Cycle Vol Loss": cycle_vol_loss, "S1 Restore Loss": restore_loss})
                 # config.track({"S1 Source Identity Loss": source_identity_loss, "S1 Vol Loss": vol_loss, "S1 Cycle Vol Loss": cycle_vol_loss, "S1 Restore Loss": restore_loss})
                 config.track({"S1 Vol Loss": vol_loss})
